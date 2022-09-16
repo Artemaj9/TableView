0a441124c5eb6e294569e1c597b60263b5a2ee4f
Artem <moiseev@physics.msu.ru>
2022-09-16 22:43:50 +0300

    Custom Table View Cell

---
+++ TableView/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  TableView
+//
+//  Created by Artem on 16.09.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
